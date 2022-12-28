79413996ed363c725ddb608984bbc73ae7b44b32
bandarichandana-2926 <bandarichandana96@gmail.com>
2022-12-28 19:03:15 +0530

    div

---
+++ Practice/Division.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+        int div = sc.nextInt();
+    if(div% 7 == 0){
+        System.out.println("given number is divisible by 7");
+
+    }
+    else{
+        System.out.println("given number is not divisible by 7");
+    }
+    }
